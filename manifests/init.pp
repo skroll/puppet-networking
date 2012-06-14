85303d38725ee7a3655013bd0581e66028b3085e
Scott M. Kroll <skroll@gmail.com>
2012-06-13 23:55:11 -0400

    Create module.

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: networking
+#
+# This module manages networking
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class networking {
+
+
+}
