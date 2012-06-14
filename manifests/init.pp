8c35178e0481fd9d6bfa1af1962a46fda8aa9451
Scott M. Kroll <skroll@gmail.com>
2012-06-14 00:15:09 -0400

    Create main networking class

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,7 +11,18 @@
 # Sample Usage:
 #
 # [Remember: No empty lines between comments and class definition]
-class networking {
-
-
+class networking (
+  $packages => undef,
+) {
+  if $packages == undef {
+    case $operatingsystem {
+      darwin: {
+        include networking::defaults::packages::darwin
+      }
+      ubuntu: {
+        include networking::defaults::packages::debian
+      }
+      default: { fail ("Don't know how to handle networking packages on $operatingsystem") }
+    }
+  }
 }
