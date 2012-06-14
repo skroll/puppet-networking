42301236f15d4553b0a973608114e3e0cc09d9db
Scott M. Kroll <skroll@gmail.com>
2012-06-14 00:25:52 -0400

    Simplify spec_helper.rb for testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,4 @@
-require 'pathname'
-dir = Pathname.new(__FILE__).parent
-$LOAD_PATH.unshift(dir, dir + 'lib', dir + '../lib')
+require 'rspec-puppet'
 
-require 'mocha'
-require 'puppet'
-gem 'rspec', '=1.2.9'
-require 'spec/autorun'
+RSpec.configure
 
-Spec::Runner.configure do |config|
-    config.mock_with :mocha
-end
-
-# We need this because the RAL uses 'should' as a method.  This
-# allows us the same behaviour but with a different method name.
-class Object
-    alias :must :should
-end
