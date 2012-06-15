2f6651d034543c8561587a6ab6896fab29361133
Scott M. Kroll <skroll@gmail.com>
2012-06-15 00:00:36 -0400

    Add spec test for ipaddr_network function

---
+++ spec/functions/ipaddr_network_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe 'ipaddr_network' do
+    tests = [
+        { :ipaddr => '192.168.6.1',   :network => '255.255.255.0',   :out => '192.168.6.0', },
+        { :ipaddr => '192.168.6.128', :network => '255.255.255.0',   :out => '192.168.6.0', },
+        { :ipaddr => '192.168.6.254', :network => '255.255.255.224', :out => '192.168.6.224', },
+        { :ipaddr => '192.168.6.181', :network => '255.255.255.224', :out => '192.168.6.160', },
+    ]
+
+    tests.each do |test|
+       it "should return '%s' with input ('%s', '%s')" % [test[:out], test[:ipaddr], test[:network]] do
+            should run.with_params(test[:ipaddr], test[:network]).and_return(test[:out])
+        end
+    end
+end
+
